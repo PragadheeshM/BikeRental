df89d53805f145d4326b9e7e3ef6856e72143b3b
PragadheeshM <pragadheeshpragadheesh75@gmail.com>
2026-05-01 11:37:43 +0530

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,7 +125,7 @@
 
     dropdownToggles.forEach(function (toggle) {
       toggle.addEventListener('click', function (e) {
-        if (window.innerWidth > 1024) return;
+        if (!window.matchMedia('(max-width: 1024px)').matches) return;
         e.preventDefault();
         var parent = toggle.closest('.navbar__item');
         parent.classList.toggle('navbar__item--open');
